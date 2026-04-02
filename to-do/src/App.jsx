f577487db872abf3e92b031be9134ed53c5bc883
icaru <creft294@gmail.com>
2026-04-02 14:40:52 -0300

    transferi alguns arquivos

---
+++ to-do/src/App.jsx
@@ -0,0 +1,62 @@
+
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [input, setInput] = useState('')
+  const [list, setList] = useState([])
+  const [filter, setFilter] = useState('all')
+
+
+  /* adicionar */
+  function adicionar() {
+    if (!input) {
+      return
+    }
+    setList([...list, { text: input, done: false }])
+    setInput('')
+  }
+
+  /* remover */
+  function remover(id) {
+    const newList = list.filter((_, tasks) => tasks !== id)
+    setList(newList)
+  }
+
+  /* logica do risco */
+  function toggle(id) {
+    const up = list.map((marcado, idx) => {
+      if (idx === id) {
+        return { ...marcado, done: !marcado.done }
+      }
+      return marcado
+    })
+    setList(up)
+  }
+  /* mostrar os resultados */
+  const filterTask = list.filter(task => {
+    if (filter === "all") return true
+    if (filter === "done") return task.done
+    if (filter === "pending") return !task.done
+  })
+
+  return (
+    <div>
+      <input type="text" placeholder='task' value={input} onChange={e => setInput(e.target.value)} />
+
+      <button onClick={adicionar}>adicionar</button>
+      <button onClick={() => setFilter("all")}>todas</button>
+      <button onClick={() => setFilter("done")}>cocluidas</button>
+      <button onClick={() => setFilter("pending")}>pendentes</button>
+
+      {filterTask.map((lista, idx) => (
+        <ul key={idx}>
+          <li onClick={() => toggle(idx)} className={lista.done ? "riscado" : ""}>{lista.text}</li>
+          <button onClick={() => remover(idx)}>remove</button>
+        </ul>
+      ))}
+      <p>tot tasks: {list.length}</p>
+    </div>
+  )
+}
+export default App
